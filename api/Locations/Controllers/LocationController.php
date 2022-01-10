8dc9626322b99ef188c358a0334bb0a6ba52012b
Nguyen Ha Trang <hatranga4k49kc@gmail.com>
2022-01-10 22:29:15 +0700

    update

--- api/Locations/Controllers/LocationController.php
+++ api/Locations/Controllers/LocationController.php
@@ -4,15 +4,17 @@ namespace Api\Locations\Controllers;
 
 use Infrastructure\Http\Controller;
 use Api\Locations\Services\LocationService;
+use Infrastructure\Libraries\Response;
 
 class LocationController extends Controller
 {
     protected $locationService;
-    
+    protected $response;
 
-    public function __construct(LocationService $locationService)
+    public function __construct(LocationService $locationService, Response $response)
     {
         $this->locationService = $locationService;
+        $this->response = $response;
     }
 
 
@@ -21,7 +23,7 @@ class LocationController extends Controller
         $resourceOption = $this->parseResourceOptions();
         // dd($resourceOption);
         $data = $this->locationService->getAll($resourceOption);
-        return $data;
+        return $this->response->renderSuccess('LWS001', $data);
     }
 
 
