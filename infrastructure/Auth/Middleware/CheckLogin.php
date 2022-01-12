e4bf9d0a581a9372021e8df44ab1c66813da8196
ninhtq <ninh.tran@amela.vn>
2022-01-12 14:48:04 +0700

    check

--- infrastructure/Auth/Middleware/CheckLogin.php
+++ infrastructure/Auth/Middleware/CheckLogin.php
@@ -1,32 +1,32 @@
 <?php
 
-    namespace Infrastructure\Auth\Middleware;
+namespace Infrastructure\Auth\Middleware;
 
-    use Illuminate\Auth\Middleware\Authenticate;
-    use Infrastructure\Libraries\HelperFunction;
-    use Illuminate\Foundation\Application;
-    use Illuminate\Routing\Route;
-    use Closure;
+use Illuminate\Auth\Middleware\Authenticate;
+use Infrastructure\Libraries\HelperFunction;
+use Illuminate\Foundation\Application;
+use Illuminate\Routing\Route;
+use Closure;
 
-    class CheckLogin extends Authenticate
-    {
-        public function __construct(
-            Route $route,
-            Application $app,
-            Authenticate $authenticate,
-            HelperFunction $helperFunction
-        ) {
-            $this->app            = $app;
-            $this->route          = $route;
-            $this->authenticate   = $authenticate;
-            $this->helperFunction = $helperFunction;
-        }
-
-        public function handle($request, Closure $next, $scopesString = null)
-        {
-            if(\Auth::check()){
-                return redirect('/admin/dashboard');
-            }
-            return $next($request);
-        }
+class CheckLogin
+{
+    public function __construct(
+        Route $route,
+        Application $app,
+        Authenticate $authenticate,
+        HelperFunction $helperFunction
+    ) {
+        $this->app            = $app;
+        $this->route          = $route;
+        $this->authenticate   = $authenticate;
+        $this->helperFunction = $helperFunction;
     }
+
+    public function handle($request, Closure $next)
+    {
+        if(\Auth::check()){
+            return redirect('/admin/dashboard');
+        }
+        return $next($request);
+    }
+}
